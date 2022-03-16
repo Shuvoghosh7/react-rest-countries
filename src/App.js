fbb06c2a5df486158a5f43a48d28c2bb5c01d808
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-17 02:08:28 +0600

    show loded data in UI

--- src/App.js
+++ src/App.js
@@ -10,22 +10,32 @@ function App() {
   );
 }
 
-function LoadCountries (){
-  const[countries,setCountries]=useState([])
-  useEffect(()=>{
+function LoadCountries() {
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-  },[])
-  return(
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+  return (
     <div>
       <h1>Visiting Every Countries in the world !!!</h1>
       <h3>Avaiable Country:{countries.length}</h3>
       {
-        countries.map(country =><p>{country.name.common}</p>)
+        countries.map(country => <Country name={country.name.common} Population={country.population}></Country>)
       }
     </div>
   )
 }
+
+function Country(props){
+return(
+  <div>
+    <h3>Name: {props.name}</h3>
+    <p>Population: {props.Population}</p>
+  </div>
+)
+}
+
 
 export default App;
